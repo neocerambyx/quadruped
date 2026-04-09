de40bef5f8d79ed4a7dc3dbfdab4d32d3cd68af9
alamtaab <167731823+neocerambyx@users.noreply.github.com>
2026-04-08 22:28:08 -0700

    Update Joint.cpp

--- firmware/lib/Joint/Joint.cpp
+++ firmware/lib/Joint/Joint.cpp
@@ -4,9 +4,25 @@ Joint::Joint(Adafruit_PWMServoDriver* driver, JointConfig config) : _driver(driv
 }
 
 void Joint::setAngle(float angle) {
+    if (!isEnabled) return; 
+
     angle = constrain(angle, _cfg.minAngle, _cfg.maxAngle);
+    
     _currentAngle = angle;
     _driver->setPWM(_cfg.id, 0, degreesToPulse(angle));
+
+}
+
+//Filtered_Angle=(α×New_Target)+((1−α)×Previous_Filtered_Angle)
+
+
+void Joint::disable() {
+    isEnabled = false;
+    _driver->setPWM(_cfg.id, 0, 4096);
+}
+
+void Joint::enable() {
+    isEnabled = true;
 }
 
 uint16_t Joint::degreesToPulse(float angle) {
